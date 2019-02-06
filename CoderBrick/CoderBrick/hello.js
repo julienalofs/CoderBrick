9e24ec4ef54a257f08d4959f1fa1d1b247b92414
Tuxbee <tuxbe@hotmail.com>
2019-02-06 21:56:10 +0100

    First Test

---
+++ CoderBrick/CoderBrick/hello.js
@@ -0,0 +1,5 @@
+﻿function ShowHelloMessage() {
+   var name = document.getElementById("myname");
+   document.getElementById("hellomessage").innerHTML = "Hello, " + name.value;
+}
+document.getElementById("mybutton").onclick = ShowHelloMessage;
