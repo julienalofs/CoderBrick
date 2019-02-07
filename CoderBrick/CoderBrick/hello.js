7fbf1da3d9d6d745f9ca6786e41711ed2e690cfb
Julien Alofs <julienalofs@gmail.com>
2019-02-07 21:51:55 +0100

    Step 1: canvas inside html file

--- CoderBrick/CoderBrick/hello.js
+++
@@ -1,5 +0,0 @@
-﻿function ShowHelloMessage() {
-   var name = document.getElementById("myname");
-   document.getElementById("hellomessage").innerHTML = "Hello, " + name.value;
-}
-document.getElementById("mybutton").onclick = ShowHelloMessage;
